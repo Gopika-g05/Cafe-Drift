609a5a3038be1d54d53abcbd608e52be2f702ef4
Gopika-g05 <gopikagirish005@gmail.com>
2026-07-18 19:11:07 +0530

    Serve frontend files from server

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 const orderRoutes = require("./src/routes/orderRoutes");
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+// 1. Serve all the static assets (CSS, JS, Images) inside the frontend folder
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// 2. Serve index.html as the main entry point for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
 app.use("/api/orders", orderRoutes);
 (async () => {
   try {
